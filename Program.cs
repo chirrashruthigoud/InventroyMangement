e27b55c285c516ce98248b6e9f8337d97cd1a900
chirrashruthigoud <122668208+chirrashruthigoud@users.noreply.github.com>
2023-02-21 15:50:17 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,12 +9,10 @@
 
             InventoryDetails data = fetchInvertory.Read(path);
            
-            for (int i = 0; i < data.typesOfRice.Count; i++)
-            {
 
                 Console.WriteLine(data.typesOfRice[i].name);
                 Console.WriteLine(data.typesOfRice[i].weight);
                 Console.WriteLine(data.typesOfRice[i].price);
-        }   }
+        }   
     }
 }
