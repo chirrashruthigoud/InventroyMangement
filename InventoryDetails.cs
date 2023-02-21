4e28d4783a47e6385144406e3ffc893ee93afd82
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-21 13:28:41 +0530

    Json Inventory Data Management of Rice

---
+++ InventoryDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class InventoryDetails
+    {
+        public List<TypesOfRice >typesOfRice;
+ 
+
+    }
+
+    public class TypesOfRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+     
+}
