5e61c7b5f5b14f78ec775194e8c0a8e1b1cf8220
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-21 13:23:02 +0530

    fetch inventory

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace InventoryManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Admin\\source\\repos\\214\\InventoryMangement\\InventoryMangement\\inventory.json";
+            FetchInvertory fetchInvertory = new FetchInvertory();
+
+            InventoryDetails data = fetchInvertory.Read(path);
+           
+            for (int i = 0; i < data.typesOfRice.Count; i++)
+            {
+
+                Console.WriteLine(data.typesOfRice[i].name);
+                Console.WriteLine(data.typesOfRice[i].weight);
+                Console.WriteLine(data.typesOfRice[i].price);
+        }   }
+    }
+}
