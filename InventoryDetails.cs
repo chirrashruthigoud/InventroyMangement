d197f29051a2ff04084d7f00940de25de36c4918
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-21 16:27:13 +0530

    Display More Items Using Array

--- InventoryDetails.cs
+++ InventoryDetails.cs
@@ -10,12 +10,28 @@ namespace InventoryManagement
     public class InventoryDetails
     {
       public List<TypesOfRice>typesOfRice;
-      }
-     public class TypesOfRice
-       {
+       
+        public List<TypesOfPulses> typesOfPulses;
+        public List<TypesOfWheat> typesOfWheat;
+
+    }
+
+    public class TypesOfRice
+    {
         public string name;
         public int weight;
         public int price;
     }
-
+    public class TypesOfPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesOfWheat
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
 }
