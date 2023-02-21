3a4737c163bd97dd725f072b61cdbf6089dc6a56
chirrashruthigoud <122668208+chirrashruthigoud@users.noreply.github.com>
2023-02-21 16:29:59 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,9 +19,7 @@ namespace InventoryManagement
                 Console.WriteLine(data.typesOfRice[i].name);
                 Console.WriteLine(data.typesOfRice[i].weight);
                 Console.WriteLine(data.typesOfRice[i].price);
-                int price = data.typesOfRice[i].weight * data.typesOfRice[i].price;
-                Console.WriteLine(" The price of {0} for {1}kg is {2}",
-                    data.typesOfRice[i].name, data.typesOfRice[i].weight, price);
+              
                 Console.WriteLine("***********************************");
             }
             Console.WriteLine("***********************************");
@@ -33,9 +31,7 @@ namespace InventoryManagement
                 Console.WriteLine(data.typesOfPulses[i].name);
                 Console.WriteLine(data.typesOfPulses[i].weight);
                 Console.WriteLine(data.typesOfPulses[i].price);
-                int price = data.typesOfPulses[i].weight * data.typesOfPulses[i].price;
-                Console.WriteLine(" The price of {0} for {1}kg is {2}",
-                    data.typesOfPulses[i].name, data.typesOfPulses[i].weight, price);
+                
                 Console.WriteLine("***********************************");
             }
             Console.WriteLine("***********************************");
@@ -47,9 +43,6 @@ namespace InventoryManagement
                 Console.WriteLine(data.typesOfWheat[i].name);
                 Console.WriteLine(data.typesOfWheat[i].weight);
                 Console.WriteLine(data.typesOfWheat[i].price);
-                int price = data.typesOfWheat[i].weight * data.typesOfWheat[i].price;
-                Console.WriteLine(" The price of {0} for {1}kg is {2}",
-                    data.typesOfWheat[i].name, data.typesOfWheat[i].weight, price);
                 Console.WriteLine("***********************************");
             }
         }
