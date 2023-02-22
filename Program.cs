a6da6016ed508daf07064b7d8ec3354f6f50ae94
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-22 21:45:46 +0530

    get data of json stock problem

--- Program.cs
+++ Program.cs
@@ -10,49 +10,10 @@ namespace InventoryManagement
 
             InventoryDetails data = fetchInvertory.Read(path);
 
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Types of Rice");
-            Console.WriteLine("***********************************");
-            for (int i = 0; i < data.typesOfRice.Count; i++)
-            {
+            Console.WriteLine(data.typesofstock.stockname);
+            Console.WriteLine(data.typesofstock.shareprice);
+            Console.WriteLine(data.typesofstock.numberofshare);
 
-                Console.WriteLine(data.typesOfRice[i].name);
-                Console.WriteLine(data.typesOfRice[i].weight);
-                Console.WriteLine(data.typesOfRice[i].price);
-                int price = data.typesOfRice[i].weight * data.typesOfRice[i].price;
-                Console.WriteLine(" The price of {0} for {1}kg is {2}",
-                    data.typesOfRice[i].name, data.typesOfRice[i].weight, price);
-                Console.WriteLine("***********************************");
-            }
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Types of Pulses");
-            Console.WriteLine("***********************************");
-            for (int i = 0; i < data.typesOfPulses.Count; i++)
-            {
-
-                Console.WriteLine(data.typesOfPulses[i].name);
-                Console.WriteLine(data.typesOfPulses[i].weight);
-                Console.WriteLine(data.typesOfPulses[i].price);
-                int price = data.typesOfPulses[i].weight * data.typesOfPulses[i].price;
-                Console.WriteLine(" The price of {0} for {1}kg is {2}",
-                    data.typesOfPulses[i].name, data.typesOfPulses[i].weight, price);
-                Console.WriteLine("***********************************");
-            }
-            Console.WriteLine("***********************************");
-            Console.WriteLine("Types of Wheat");
-            Console.WriteLine("***********************************");
-            for (int i = 0; i < data.typesOfPulses.Count; i++)
-            {
-
-                Console.WriteLine(data.typesOfWheat[i].name);
-                Console.WriteLine(data.typesOfWheat[i].weight);
-                Console.WriteLine(data.typesOfWheat[i].price);
-                int price = data.typesOfWheat[i].weight * data.typesOfWheat[i].price;
-                Console.WriteLine(" The price of {0} for {1}kg is {2}",
-                    data.typesOfWheat[i].name, data.typesOfWheat[i].weight, price);
-                Console.WriteLine("***********************************");
-            }
         }
-
     }
 }
